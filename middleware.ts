895e5cbe882bdea9561f24d72a32413d19b6646e
Lorenzo <henriquewillians@usp.br>
2024-10-31 19:39:32 -0300

    oh man

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  if (req.nextUrl.pathname.startsWith('/api')) {
+    res.headers.set('Access-Control-Allow-Origin', '*');
+    res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.headers.set('Access-Control-Allow-Credentials', 'true');
+  }
+
+  return res;
+}
+
+// Define o matcher para interceptar apenas rotas de API
+export const config = {
+  matcher: '/api/:path*',
+};
---
+++ next.config.js
@@ -0,0 +1,17 @@
+// next.config.js
+module.exports = {
+    async headers() {
+      return [
+        {
+          source: '/api/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,POST,PUT,DELETE' },
+            { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+          ],
+        },
+      ];
+    },
+  };
+  
