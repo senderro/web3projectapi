f0e6bfa5161c02bcb924e858e0a3d126f9e2d087
Lorenzo <henriquewillians@usp.br>
2024-10-31 19:05:28 -0300

    testing cors confs

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",  // Aplica CORS para todas as rotas de API
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Substitua '*' pelo domínio específico, se necessário
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ];
+  },
+};
