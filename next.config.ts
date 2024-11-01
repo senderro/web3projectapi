04e5d5d2dc4b1740655880ef3125780777e6dd3b
Lorenzo <henriquewillians@usp.br>
2024-11-01 20:40:53 -0300

    teste cors

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,20 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
-
+  async headers() {
+    return [
+      {
+        // Isso define os cabeçalhos CORS para todas as rotas API
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Accept, Accept-Version, Content-Length, Content-Type, Authorization' },
+        ],
+      },
+    ]
+  },
 };
 
 
