834aa694e25d01edc1d1a6064add448c42e190cf
Lorenzo <henriquewillians@usp.br>
2024-11-01 21:54:48 -0300

    testing

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,18 @@ import { NextResponse, type NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
     console.log(req.nextUrl);
     const res = NextResponse.next();
-    res.headers.append("ACCESS-CONTROL-ALLOW-ORIGIN", "*");
+    
+    res.headers.append('Access-Control-Allow-Credentials', 'true');
+    res.headers.append('Access-Control-Allow-Origin', '*'); // replace this with your actual origin
+    res.headers.append('Access-Control-Allow-Methods', 'GET, DELETE, PATCH, POST, PUT');
+    res.headers.append(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
     return res;
 }
+
 
 export const config = {
     matcher: ["/api/:path*"]
