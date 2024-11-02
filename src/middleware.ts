507f1539fd3f9021278f4935df2b43708a065c57
Lorenzo <henriquewillians@usp.br>
2024-11-01 21:50:33 -0300

    oh

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
+    console.log(req.nextUrl);
     const res = NextResponse.next();
     res.headers.append("ACCESS-CONTROL-ALLOW-ORIGIN", "*");
     return res;
