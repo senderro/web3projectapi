aa3a2e4f9081020d237e94819b22ff4da7dbab33
Lorenzo <henriquewillians@usp.br>
2024-11-11 23:18:30 -0300

    remove unecessary

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from 'next/server';
-import prisma from '../../../../lib/prisma';
 import { IAuth } from '@/interfaces';
 import * as rippleKeypairs from 'ripple-keypairs';
 
